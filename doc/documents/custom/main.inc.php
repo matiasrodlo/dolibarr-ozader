047c54086f346f9e6f8103789f9589d102d98e85
Matías Rodlo <m@hablaqui.com>
2023-09-16 22:20:05 -0300

    01/2018
    Last modified: 01/2018

---
+++ doc/documents/custom/main.inc.php
@@ -0,0 +1,3 @@
+<?php
+// Wrapper to include main into htdocs
+include_once '/home/meyzjcmc/public_html/dolibarr/htdocs/main.inc.php';
---
+++ doc/documents/custom/master.inc.php
@@ -0,0 +1,3 @@
+<?php
+// Wrapper to include master into htdocs
+include_once '/home/meyzjcmc/public_html/dolibarr/htdocs/master.inc.php';
